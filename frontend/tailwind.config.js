0c3115bde02d4570aba0189b11ea0c852710c7cd
Kamya <kamyakumari140@gmail.com>
2025-04-20 23:08:20 +0530

    added tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",             
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#1F7A1F',  // deep green
-        accent: '#F7C948',   // warm yellow
+        primary: "#1F7A1F",
+        accent: "#F7C948",
       },
     },
   },
