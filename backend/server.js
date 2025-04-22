4f3b8fed4f9b79a273e09755002b547710ca85f1
Gorav <goravjindal86@gmail.com>
2025-04-22 16:57:57 +0530

    linking backend with frontend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: '*', 
+  origin: 'https://green-world-delta.vercel.app/', 
   methods: 'GET,POST,PUT,DELETE', 
   credentials: true
 }));
