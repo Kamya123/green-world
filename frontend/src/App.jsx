b7917aafdef362c2ca252a9728a0e08cd2e61369
Kamya <kamyakumari140@gmail.com>
2025-04-22 15:16:09 +0530

    Signup -> SignUp in App.jsx to resolve connfllicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import SignUp from './pages/Signup';
+import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import OTPVerification from './pages/OtpVerification';
 
