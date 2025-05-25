af9cc3426e9559bbc001dd658ad38cbe627477fc
Kamya <kamyakumari140@gmail.com>
2025-05-25 13:48:59 +0530

    added new image

--- frontend/src/components/landingPage/ImageSlider.jsx
+++ frontend/src/components/landingPage/ImageSlider.jsx
@@ -30,7 +30,7 @@ const slides = [
     caption: 'Ensuring healthy cattle management.'
   },
   {
-    src: 'https://images.unsplash.com/photo-1622123651884-e3d30eebfbff?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'https://images.unsplash.com/photo-1623211268529-69c56e303312?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     alt: 'Farmers working together',
     title: 'Community Farming',
     caption: 'Uniting hands to grow sustainable harvests.'
